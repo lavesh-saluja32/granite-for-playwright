fb844a219c5cd0974fed6ee65ab42bb497896cc1
Lavesh Saluja <lavesh.saluja@bigbinary.com>
2026-03-30 16:18:14 +0530

    Added all tests for tasks and multi tab test for role based test

--- playwright-tests/e2e/poms/tasks.ts
+++ playwright-tests/e2e/poms/tasks.ts
@@ -1,5 +1,13 @@
 import { Page, expect } from "@playwright/test";
 
+interface TaskName {
+    taskName: string;
+  }
+
+  interface CreateNewTaskProps extends TaskName {
+    userName?: string;
+  }
+
 export default class TaskPage {
   page: Page;
 
@@ -7,14 +15,14 @@ export default class TaskPage {
     this.page = page;
   }
 
-  createTaskAndVerify = async ({ taskName }: { taskName: string }) => {
+  createTaskAndVerify = async ({ taskName, userName = "Oliver Smith", }: CreateNewTaskProps) => {
     await this.page.getByTestId("navbar-add-todo-link").click();
     await this.page.getByTestId("form-title-field").fill(taskName);
 
     await this.page.locator(".css-2b097c-container").click();
     await this.page
       .locator(".css-26l3qy-menu")
-      .getByText("Oliver Smith")
+      .getByText(userName)
       .click();
     await this.page.getByTestId("form-submit-button").click();
     const taskInDashboard = this.page
@@ -24,5 +32,29 @@ export default class TaskPage {
       });
     await taskInDashboard.scrollIntoViewIfNeeded();
     await expect(taskInDashboard).toBeVisible();
+  };
+  markTaskAsCompletedAndVerify = async ({ taskName }: TaskName) => {
+    await this.page
+      .getByTestId("tasks-pending-table")
+      .getByRole("row", { name: taskName })
+      .getByRole("checkbox")
+      .click();
+    const completedTaskInDashboard = this.page
+      .getByTestId("tasks-completed-table")
+      .getByRole("row", { name: taskName });
+    await completedTaskInDashboard.scrollIntoViewIfNeeded();
+    await expect(completedTaskInDashboard).toBeVisible();
+  };
+
+  starTaskAndVerify = async ({ taskName }: TaskName) => {
+    const starIcon = this.page
+      .getByTestId("tasks-pending-table")
+      .getByRole("row", { name: taskName })
+      .getByTestId("pending-task-star-or-unstar-link");
+    await starIcon.click();
+    await expect(starIcon).toHaveClass(/ri-star-fill/i);
+    await expect(
+      this.page.getByTestId("tasks-pending-table").getByRole("row").nth(1)
+    ).toContainText(taskName);
   };
 }
--- playwright-tests/e2e/tests/tasks.spec.ts
+++ playwright-tests/e2e/tests/tasks.spec.ts
@@ -1,10 +1,10 @@
 import { test } from "../fixtures";
 import { expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
-
+import LoginPage from "../poms/login"
 test.describe("Tasks page", () => {
   let taskName: string;
-  test.beforeEach(async ({page}) => {
+  test.beforeEach(async ({ page }) => {
     taskName = faker.word.words({ count: 5 });
     await page.goto("/");
   });
@@ -23,15 +23,79 @@ test.describe("Tasks page", () => {
     taskPage,
   }) => {
     await taskPage.createTaskAndVerify({ taskName });
-    await page
-      .getByTestId("tasks-pending-table")
-      .getByRole("row", { name: taskName })
-      .getByRole("checkbox")
-      .click();
+    await taskPage.markTaskAsCompletedAndVerify({ taskName });
+  });
+
+  test("should be able to delete a completed task", async ({
+    page,
+    taskPage,
+  }) => {
+    await taskPage.createTaskAndVerify({ taskName });
+    await taskPage.markTaskAsCompletedAndVerify({ taskName });
     const completedTaskInDashboard = page
       .getByTestId("tasks-completed-table")
       .getByRole("row", { name: taskName });
-    await completedTaskInDashboard.scrollIntoViewIfNeeded();
-    await expect(completedTaskInDashboard).toBeVisible();
+
+    await completedTaskInDashboard
+      .getByTestId("completed-task-delete-link")
+      .click();
+
+    await expect(completedTaskInDashboard).toBeHidden();
+    await expect(
+      page
+        .getByTestId("tasks-pending-table")
+        .getByRole("row", { name: taskName })
+    ).toBeHidden();
   });
+
+  test.describe("Starring tasks feature", () => {
+    test.describe.configure({ mode: "serial" });
+
+    test("should be able to star a pending task", async ({
+      page,
+      taskPage,
+    }) => {
+      await taskPage.createTaskAndVerify({ taskName });
+      await taskPage.starTaskAndVerify({ taskName });
+    });
+
+    test("should be able to un-star a pending task", async ({
+      page,
+      taskPage,
+    }) => {
+      await taskPage.createTaskAndVerify({ taskName });
+      await taskPage.starTaskAndVerify({ taskName });
+      const starIcon = page
+        .getByTestId("tasks-pending-table")
+        .getByRole("row", { name: taskName })
+        .getByTestId("pending-task-star-or-unstar-link");
+      await starIcon.click();
+      await expect(starIcon).toHaveClass(/ri-star-line/);
+    });
+   });
+
+  test("should create a new task with a different user as the assignee", async ({page,browser,taskPage})=>{
+    await taskPage.createTaskAndVerify({ taskName, userName: "Sam Smith" });
+
+    const newUserContext = await browser.newContext({
+        storageState:{cookies:[],origins:[]},
+    })
+    const newUserPage = await newUserContext.newPage();
+
+    const loginPage = new LoginPage(newUserPage);
+
+    await newUserPage.goto("/");
+    await loginPage.loginAndVerifyUser({
+        email: "sam@example.com",
+        password: "welcome",
+        username: "Sam Smith",
+      });
+      await expect(
+        newUserPage
+          .getByTestId("tasks-pending-table")
+          .getByRole("row", { name: taskName })
+      ).toBeVisible();
+      await newUserPage.close();
+    await newUserContext.close();
+  })
 });
